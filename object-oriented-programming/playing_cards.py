bb8e2c3eacd14edd4d1b6be2b8d9b6bd9b8ffe6f
shayaltshue <shayaltshue@gmail.com>
2020-07-24 20:42:41 -0500

    continued "playing" with the playing cards class

--- object-oriented-programming/playing_cards.py
+++ object-oriented-programming/playing_cards.py
@@ -5,9 +5,27 @@ class Card:
         self.number = number
     
     def print_description(self):
-        if self.number == 0:
-            print('Ace of {}'.format(self.suit))
+        if self.is_joker == False:
+            # Checks for non-joker face cards (Ace, King, Queen, Jack)
+            if self.number in [0, 13, 12, 11]:
+                if self.number == 0:
+                    print('Ace of {}'.format(self.suit))
+                elif self.number == 13:
+                    print('King of {}'.format(self.suit))
+                elif self.number == 12:
+                    print('Queen of {}'.format(self.suit))
+                elif self.number == 11:
+                    print('Jack of {}'.format(self.suit))
+            # If not a joker and not a face card, print the number and the suit
+            else:
+                print('{} of {}'.format(self.number, self.suit))
+        else:
+            print('Card is a Joker')
 
 ace_spades = Card(False, 'spades', 0)
+queen_hearts = Card(False, 'hearts', 12)
+nine_diamonds = Card(False, 'diamonds', 9)
 
 ace_spades.print_description()
+queen_hearts.print_description()
+nine_diamonds.print_description()
